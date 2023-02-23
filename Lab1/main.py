36cd98ce61cd66e48b6a8788c8a3375c613e2ac9
Egor <egor@VivoBook-ASUSLaptop-X513IA-D513IA-3379996d.localdomain>
2023-02-23 04:45:03 +0300

    Added task with even numbers to main.py

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,5 +1,5 @@
 from calculation import calc_func
-
+from random import randint
 
 print("Hello world")
 
@@ -9,5 +9,11 @@ try:
     value2 = int(input("Enter the second value..\n"))
 
     print(f"Output: {calc_func(value1, value2, op)}")
+
 except Exception as exception:
     print(exception.args)
+
+primaryList = [randint(1, 10) for i in range(10)]
+print(f"Primary list: {primaryList}")
+finList = [i for i in primaryList if i % 2 == 0]
+print(f"Final list with even numbers: {finList}")
