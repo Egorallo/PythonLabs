d87ba986bd3202be38f2062e25119413abb50a95
Egor <egor@VivoBook-ASUSLaptop-X513IA-D513IA-3379996d.localdomain>
2023-02-23 04:30:24 +0300

    Updated main.py according to the task

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,1 +1,13 @@
-print("Hello")
+from calculation import calc_func
+
+
+print("Hello world")
+
+try:
+    value1 = int(input("Enter the first value..\n"))
+    op = input("Enter the operation required..\n")
+    value2 = int(input("Enter the second value..\n"))
+
+    print(f"Output: {calc_func(value1, value2, op)}")
+except Exception as exception:
+    print(exception.args)
