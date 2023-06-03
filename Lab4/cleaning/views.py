b3051675102277e0779c9a80874cb2f39e950453
Egorallo <sea25042003@gmail.com>
2023-06-03 17:40:21 +0300

    some basic views work

--- Lab4/cleaning/views.py
+++ Lab4/cleaning/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from .models import ServicePack, Order, ServicePackInstance, Service
 # Create your views here.
+
+
+def index(request):
+    """View function for home page of site."""
+
+    num_service_packs = ServicePack.objects.all().count()
+    num_instances = ServicePackInstance.objects.all().count()
+    num_instances_available = ServicePackInstance.objects.filter(status__exact='a').count()
+    num_orders = Order.objects.count()
+
+    context = {
+        'num_service_packs': num_service_packs,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_orders': num_orders,
+    }
+
+    return render(request, 'index.html', context=context)
