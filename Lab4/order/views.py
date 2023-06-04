ed278d7a740142a26391c9e573a357843c4b6adb
Egorallo <sea25042003@gmail.com>
2023-06-05 00:09:26 +0300

    total cost of orders

--- Lab4/order/views.py
+++ Lab4/order/views.py
@@ -1,6 +1,8 @@
+from django.db.models import Sum
 from django.shortcuts import render
 from django.views import generic
 
+from cleaning.models import ServicePack
 from .models import OrderItem
 from cart.cart import Cart
 # from cleaning.models import Client
@@ -23,7 +25,6 @@ def order_create(request):
                                      quantity=item['quantity'])
             item['servicepack'].purchase_count += item['quantity']
             item['servicepack'].save()
-
         cart.clear()
         return render(request, 'order/created.html',
                       {'order': order})
@@ -34,6 +35,26 @@ def order_create(request):
 
 class OrderListView(generic.ListView):
     model = Order
+    template_name = 'order/order_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Retrieve the list of clients and products in alphabetical order
+        client_list = Order.objects.order_by('client').values_list('client', flat=True).distinct()
+        product_list = ServicePack.objects.order_by('naming')
+
+        # Calculate the total sales amount and iterate through each order to get total cost
+        total_sales = 0
+        order_list = self.get_queryset()
+        for order in order_list:
+            total_sales += order.get_total_cost()
+
+        context['client_list'] = client_list
+        context['product_list'] = product_list
+        context['total_sales'] = total_sales
+
+        return context
 
 
 class OrderDetailView(generic.DetailView):
